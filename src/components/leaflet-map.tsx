25f4ca085f3839e5d2e4bf2c85b92d986f66cc99
Sipho Ndlalane <siphonhata@gmail.com>
2025-07-03 18:12:42 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/components/leaflet-map.tsx
+++ src/components/leaflet-map.tsx
@@ -2,16 +2,8 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-// Custom SVG icon to avoid issues with default markers
-const iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%232563EB" width="32px" height="32px"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
-const customIcon = new L.Icon({
-    iconUrl: `data:image/svg+xml;utf8,${iconSvg}`,
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32],
-});
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
 
 interface LeafletMapProps {
     lat: number;
@@ -27,7 +19,7 @@ const LeafletMap = ({ lat, lng }: LeafletMapProps) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position} icon={customIcon}>
+            <Marker position={position}>
                 <Popup>
                     Parcel's estimated location.
                 </Popup>
